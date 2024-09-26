ae7e8e8dc85622ac4953d4993c936ba59d81fc21
Justin Hunt <bitwalkerjapan@gmail.com>
2024-09-26 14:29:20 +0900

    version bump

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_poodll';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.4 (Build 2024040500)';
+$plugin->release = '1.0.5 (Build 2025092500)';
 $plugin->requires = 2022112800;//Moodle 4.1.0
-$plugin->version = 2024040500;
+$plugin->version = 2025092500;
